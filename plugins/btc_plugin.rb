c67ccdbbd5b076337dbcb070d1634eeac931ad92
kaiks <karol.lewcio@gmail.com>
2018-02-02 22:21:24 +0100

    btc plugin: add price monitoring

--- plugins/btc_plugin.rb
+++ plugins/btc_plugin.rb
@@ -1,11 +1,13 @@
 require 'open-uri'
 require 'json'
+require 'time'
 
 #TODO: extract api from user interface
 
 class BtcPlugin
   CRYPTOCOMPARE_DOMAIN = 'https://min-api.cryptocompare.com'.freeze
   BCINFO_DOMAIN = 'https://blockchain.info'.freeze
+  MSG_CHANNEL = '#kx'
   include Cinch::Plugin
 
   self.prefix = '.'
@@ -16,14 +18,44 @@ class BtcPlugin
   match /crypto ([a-z0-9]+)/i,          method: :crypto
   match /cryptoupdate/i,                method: :update
 
+  timer 600, method: :btc_price_check
+
   def initialize(*args)
     update_cryptocompare_coin_list
     super
   end
 
+  def round_to_500(price)
+    ((2.0*price).round(-3))/2.0
+  end
+
+  def btc_price_update?(current)
+    current_rounded = round_to_500(current)
+    (current_rounded > @btc_price_checkpoint && current >= (@btc_price_checkpoint + 500)) ||
+      (current_rounded < @btc_price_checkpoint && current <= (@btc_price_checkpoint - 500))
+  end
+
+  def btc_price_check
+    @btc_price_checkpoint ||= round_to_500(btc_price_in_usd)
+    current_price = btc_price_in_usd
+    current_rounded_price = round_to_500(current_price)
+    if btc_price_update?(current_price)
+      color = current_rounded_price > @btc_price_checkpoint ? Text::GREEN : Text::RED
+      update_msg = "BTC price update: #{Text.bold(Text.color(current_price, color))}, was #{@btc_price_checkpoint}"
+
+      Channel(MSG_CHANNEL).send(update_msg)
+      @btc_price_checkpoint = round_to_500(current_price)
+    end
+  end
+
+  def btc_price_in_usd
+    api_response = open("#{BCINFO_DOMAIN}/tobtc?currency=USD&value=1").read
+    1.0/api_response.to_f
+  end
+
   def btc(m)
     api_response = open("#{BCINFO_DOMAIN}/tobtc?currency=USD&value=1").read
-    m.reply (1.0/api_response.to_f).to_s + ' USD'
+    m.reply "#{btc_price_in_usd} USD"
   end
 
   def get_coin_cryptocompare(coin, currencies = %w[USD PLN])

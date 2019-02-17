06c18108487eed46b60b60c3e4f8ee4b4fe03fe5
kaiks <karol.lewcio@gmail.com>
2019-02-17 19:58:39 +0100

    Fix flood protection

--- extensions/flood_protection.rb
+++ extensions/flood_protection.rb
@@ -14,8 +14,7 @@ module Cinch
       slices.each_with_index do |slice, i|
         reply slice
         sleep(REPLY_DELAY)
-        reply '(...) - message cut off due to excess length'
-        return if i == 4
+        return (reply '(...) - message cut off due to excess length') if i == 4
       end
     end
   end

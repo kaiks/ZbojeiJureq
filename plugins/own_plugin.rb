ee566bfcbfc8dd0d39fc7e2a242997096ad8605f
kaiks <karol.lewcio@gmail.com>
2019-03-04 17:45:42 +0100

    Own plugin: minor refactoring, fix code block limits

--- plugins/own_plugin.rb
+++ plugins/own_plugin.rb
@@ -38,66 +38,62 @@ class OwnPlugin
   def on_join(m)
     user = OwnRecord.where(nick: m.user.nick).first
 
-    unless user.nil? || user.own_stage.nil?
-      if user.own_stage > 1
-        user.own_stage -= 1
-        m.channel.kick(m.user, "##{OwnConfig::KICKS + 1 - user.own_stage}")
-        user.save
-      elsif user.own_stage == 1
-        user.own_stage -= 1
-        user.save
-        m.channel.send "#{m.user.nick} was pwnz0r3d by #{user.last_owned_by} & #{bot.nick}!"
-      end
+    return if user.nil? || user.own_stage.nil?
+    
+    if user.own_stage > 1
+      user.own_stage -= 1
+      m.channel.kick(m.user, "##{OwnConfig::KICKS + 1 - user.own_stage}")
+      user.save
+    elsif user.own_stage == 1
+      user.own_stage -= 1
+      user.save
+      m.channel.send "#{m.user.nick} was pwnz0r3d by #{user.last_owned_by} & #{bot.nick}!"
     end
   end
 
   def own(m, nick)
-    if m.user.level > 0
-      owner = OwnRecord.where(:nick => m.user.nick).first
+    return unless m.user.level > 0
 
+    owner = OwnRecord.where(:nick => m.user.nick).first
+    if owner.nil?
+      OwnRecord.create( :nick => nick,
+                        :owning_times => 1
+      )
+    else
+      owner.owning_times += 1
+      owner.save
+    end
 
-      if owner.nil?
-        OwnRecord.create( :nick => nick,
-                          :owning_times => 1
-        )
-      else
-        owner.owning_times += 1
-        owner.save
-      end
+    owned = OwnRecord.where(nick: nick).first
 
-      owned = OwnRecord.where(nick: nick).first
-
-      if owned.nil?
-        OwnRecord.create(nick: nick,
-                         last_owned_time: Time.now,
-                         last_owned_by: m.user.nick,
-                         own_stage: OwnConfig::KICKS,
-                         owned_times: 1)
-      elsif (Time.now - owned.time).to_i > OwnConfig::TIME_LIMIT
-          owned.owned_times += 1
-          owned.last_owned_time = Time.now
-          owned.own_stage = OwnConfig::KICKS
-          owned.last_owned_by = m.user.nick
-          owned.save
-          user = m.channel.get_user(nick)
-          m.channel.kick(user, "#1")
-        else
-          owner.owning_times -= 1
-          owner.save
-          m.reply "Daj mu juz spokoj #{m.user.nick} :("
-        end
-      end
+    if owned.nil?
+      OwnRecord.create(nick: nick,
+                        last_owned_time: Time.now,
+                        last_owned_by: m.user.nick,
+                        own_stage: OwnConfig::KICKS,
+                        owned_times: 1)
+    elsif (Time.now - owned.time).to_i > OwnConfig::TIME_LIMIT
+        owned.owned_times += 1
+        owned.last_owned_time = Time.now
+        owned.own_stage = OwnConfig::KICKS
+        owned.last_owned_by = m.user.nick
+        owned.save
+        user = m.channel.get_user(nick)
+        m.channel.kick(user, "#1")
+    else
+      owner.owning_times -= 1
+      owner.save
+      m.reply "Daj mu juz spokoj #{m.user.nick} :("
     end
   end
 
 
   def unown(m, nick)
-    if (m.user.nick == nick || m.user.level > 0)
-      owned = OwnRecord.where(nick: nick).first
-      owned.own_stage = 0
-      owned.save
-      m.reply "Done."
-    end
+    return unless m.user.nick == nick || m.user.level > 0
+    owned = OwnRecord.where(nick: nick).first
+    owned.own_stage = 0
+    owned.save
+    m.reply "Done."
   end
 
 
@@ -115,5 +111,4 @@ class OwnPlugin
     m.channel.send 'To own a nick, type .own [nick]. To unown a nick, type .unown [nick].'
     m.channel.send 'To display owning stats, type .own_stats'
   end
-
 end

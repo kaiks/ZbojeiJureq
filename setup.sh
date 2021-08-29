957421ae234ab419042984c59d18bb5cc6f76ed4
kaiks <karol.lewcio@gmail.com>
2021-08-29 16:38:26 +0200

    Update docker instructions

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 # docker build --pull --rm -f "Dockerfile" -t zbojeijureq:latest "." <
-docker build -t zboje .
-docker run -it zboje ruby main.rb
+docker build -t zbojeijureq .
+
+# docker run -it \
+#   --name zbojeijureq \
+#   --mount source=db,target=/ZbojeiJureq/db \
+#   --mount source=logs,target=/ZbojeiJureq/logs \
+#   ruby main.rb
+
+### this works at least on windows
+docker run -e TZ=Europe/Berlin --mount source=db,target=/ZbojeiJureq/db --mount source=logs,target=/ZbojeiJureq/logs --net=host -p 6667:6667 -it zbojeijureq

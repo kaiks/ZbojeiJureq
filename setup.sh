3e1efe75e36d3298118a9cfb1a9ac9a919322b9c
kaiks <karol.lewcio@gmail.com>
2022-03-30 18:13:49 +0200

    setup file: better support for windows

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
-# docker build --pull --rm -f "Dockerfile" -t zbojeijureq:latest "." <
+docker container rm -f zbojeijureq
 docker build -t zbojeijureq .
 
-# docker run -it \
-#   --name zbojeijureq \
-#   --mount source=db,target=/ZbojeiJureq/db \
-#   --mount source=logs,target=/ZbojeiJureq/logs \
-#   ruby main.rb
-
-### this works at least on windows
-docker run -e TZ=Europe/Berlin --mount source=db,target=/ZbojeiJureq/db --mount source=logs,target=/ZbojeiJureq/logs --net=host -p 6667:6667 -it zbojeijureq
+# check if we're on windows
+if [[ "$(uname)" =~ NT ]]; then
+    echo "Windows detected"
+    export MSYS_NO_PATHCONV=1
+    container_path="$(pwd)"
+    echo "Container path: $container_path"
+    docker run -e TZ=Europe/Berlin -v "$container_path/db":"/ZbojeiJureq/db":Z -v "$container_path/logs":"/ZbojeiJureq/logs":Z -v "$HOME/www/logs":"/log_upload":Z --net=host --restart unless-stopped --name zbojeijureq -it zbojeijureq
+else
+    docker run -e TZ=Europe/Berlin --mount source=db,target=/ZbojeiJureq/db --mount source=logs,target=/ZbojeiJureq/logs --mount source=$HOME/www/logs,target=/log_upload --net=host --restart unless-stopped --name zbojeijureq -it zbojeijureq
+fi
